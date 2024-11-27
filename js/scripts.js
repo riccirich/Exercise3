c3849c648e72888612227f6e055457105054f7e7
ricci <dagta.io@gmail.com>
2024-11-27 10:55:47 -0700

    clone

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
+// buttons
 const abductBtn = document.getElementById('abduct-btn');
 const cloneBtn = document.getElementById('clone-btn');
 const flipBtn = document.getElementById('flip-btn');
 const paintBtn = document.getElementById('paint-btn');
 const partyBtn = document.getElementById('party-btn');
-const cow = document.getElementById('cow');
 
+// cows
+const cow = document.getElementById('cow');
+const cow01 = document.getElementById('cow01');
+const cow02 = document.getElementById('cow02');
+
+// abduct button
 abductBtn.addEventListener('click', () => {
     cow.classList.toggle('abducted');
 
@@ -15,6 +21,41 @@ abductBtn.addEventListener('click', () => {
     }
 });
 
+// clone button
+let clickCount = 0;
+
+cloneBtn.addEventListener('click', () => {
+    clickCount++;
+
+    switch (clickCount) {
+        case 1:
+            cow01.style.display = 'block';
+            setTimeout(() => {
+                cow01.style.opacity = '1';
+            }, 10);
+            cloneBtn.textContent = 'Clone Again!';
+            break;
+        case 2:
+            cow02.style.display = 'block';
+            setTimeout(() => {
+                cow02.style.opacity = '1';
+            }, 10);
+            cloneBtn.textContent = 'Enough Mootildas!!!';
+            break;
+        case 3:
+            cow01.style.opacity = '0';
+            cow02.style.opacity = '0';
+            setTimeout(() => {
+                cow01.style.display = 'none';
+                cow02.style.display = 'none';
+            }, 500);
+            cloneBtn.textContent = 'Clone';
+            clickCount = 0;
+            break;
+    }
+});
+
+// flip button
 flipBtn.addEventListener('click', () => {
     cow.classList.toggle('flipped');
 
@@ -24,5 +65,6 @@ flipBtn.addEventListener('click', () => {
         flipBtn.textContent = 'Do a Flip';
     }
 });
+
 
 console.log('Hello, World!');
